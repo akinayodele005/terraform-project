e51f0496e2d0420d7f180ad16511dc8366f04a61
Festus Akinola <akinayodele005@gmail.com>
2023-09-21 21:36:40 -0600

    added resources

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
-{
+terraform {
  required_providers {
     aws = {
       source = "hashicorp/aws"
       version = "~>5.16.2"
     }
   }
+}
+
+provider "aws" {
+  region ="us-east-1"
+}
+
+resources "aws_instance" "example" {
+    ami        = "ami-024e6efaf93d85776" # ubuntu 20.04 LTS // us-east-1
+    instance_type = "t2.micro"
 }
